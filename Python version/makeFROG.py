c384d48062fa8ecc9c1c1aed57ec84a87479dcab
Kabat <kabacinskipiotr@protonmail.com>
2018-01-07 14:45:01 +0100

    python draft

---
+++ Python version/makeFROG.py
@@ -0,0 +1,20 @@
+def makeFROG(electricField):
+
+N = length(electricField);
+electricFROG = electricField*(electricField.');
+
+# row rotation
+for n=2:N
+	electricFROG(n,:) = circshift(electricFROG(n,:), [0 1-n]);
+end
+
+# permute the columns to the right order
+electricFROG=fliplr(fftshift(electricFROG,2));
+
+# FFT each column and put 0 frequency in the correct place:
+electricFROG=circshift(fft(electricFROG,[],1),ceil(N/2)-1);
+
+# generate FROG trace (= |field|^2)
+intensityFROG = abs(electricFROG).^2;
+
+return [intensityFROG, electricFROG]
---
+++ Python version/testFROG.py
@@ -0,0 +1,43 @@
+clc;
+clear;
+close all;
+
+# prepare FROG trace from pulse retrieved by Femtosoft FROG
+Pulse = dlmread('..\testfrog\result.txt');
+Time = Pulse(:,1);
+Intensity = Pulse(:,2);
+Phase = Pulse(:,3);
+[computedFROG, electricFROG] = makeFROG(sqrt(Intensity).*exp(1i.*Phase));
+
+# input parameters for FROG algorithm
+errorTolerance = 8e-6;
+maxIterations = 500;
+deltaDelay = 6.515;
+whichMethod = 1;
+hidePlots = 0;
+useBootstrap = 0;
+
+# main
+howMany = 1;
+for n=1:howMany
+
+    [retrievedPulse, retrievedFROG, finalGError, finalIterations] = mainFROG(computedFROG, errorTolerance, maxIterations, deltaDelay, whichMethod, hidePlots, useBootstrap);
+
+    retrievedIntensity = abs(retrievedPulse).^2;
+    retrievedIntensity = retrievedIntensity/max(retrievedIntensity);
+    retrievedPhase = angle(retrievedPulse);
+    retrievedPhase(retrievedIntensity<0.1) = 0; % phase blanking
+
+    outputFile = [Time, retrievedIntensity, retrievedPhase];
+    method = 'power';
+    dlmwrite(['.\output_' method '\'' num2str(i) '.txt'],outputFile,'\t');
+
+end
+
+# compare retrieved pulses
+# for n=1:howMany
+#     file = dlmread(['.\output_' method '\' num2str(i) '.txt']);
+#     figure()
+#     plot(file(:,2))
+#     hold on
+# end
