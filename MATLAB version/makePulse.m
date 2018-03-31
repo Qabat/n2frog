08bed2bb95bb10ec6f1bdb505ddf7f328b180c9f
Kabat <kabacinskipiotr@protonmail.com>
2018-03-31 21:04:20 +0200

    error bars
    fully functional frog algorithm
    pulse is calculated as weighted average of 100 runs
    error is calculated as standard deviation of 100 runs with bootstrap

--- MATLAB version/makePulse.m
+++ MATLAB version/makePulse.m
@@ -18,19 +18,14 @@ for n = 2:N
 	electricFROG(n,:) = circshift(electricFROG(n,:), [0, (n-1)]);
 end
 
-% Anti-alias in time domain. See makeFROG for explanation.
-% electricFROG=electricFROG-tril(electricFROG,-ceil(N/2))-triu(electricFROG,ceil(N/2));
-
-% now electricFROG is in the "outer product form", see Kane1999
+% now electricFROG is in the outer product form
 if (whichMethod == 0)
     % Power method
 	outputPulse = electricFROG*(electricFROG'*lastPulse);
 else
     % SVD method
-    % check if this part is ok
-% 	[U, S, V] = svd(electricFROG,'econ');
-    [U, S, V] = svds(electricFROG,3);
-	outputPulse = U(:,1);
+    [U, S, V] = svds(electricFROG,1);
+	outputPulse = U;
 end
 
 % normalize to Euclidean norm 1
